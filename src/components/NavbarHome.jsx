36840bda68f47b8ab87695ac634317ae573a8f2c
Hansel-Sabu <er.hanselsabu25@gmail.com>
2025-01-23 22:06:44 +0530

    Commited

--- src/components/NavbarHome.jsx
+++ src/components/NavbarHome.jsx
@@ -3,22 +3,25 @@ import React from 'react'
 const NavbarHome = () => {
   return (
     <div>
-        <nav class="navbar navbar-expand-lg bg-primary">
-            <div class="container-fluid">
-              <a class="navbar-brand" href="#">TIST</a>
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav">
-                  <a class="nav-link active" aria-current="page" href="/">Home</a>
-                  <a class="nav-link" href="/a">Admin</a>
-                  <a class="nav-link" href="/s">Security Guards</a>
-                  <a class="nav-link disabled" aria-disabled="true"></a>
-                </div>
-              </div>
+      <nav class="navbar navbar-expand-lg bg-primary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">TIST</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+              <a class="nav-link active" aria-current="page" href="/">Home</a>
+              <a class="nav-link" href="/a">Admin</a>
+              <a class="nav-link" href="/s">Security Guards</a>
+              <a class="nav-link disabled" aria-disabled="true"></a>
             </div>
-          </nav>
+            <form class="d-flex" role="search">
+              <button class="btn btn-outline-success me-2" type="submit">LogOut</button>
+            </form>
+          </div>
+        </div>
+      </nav>
     </div>
   )
 }
