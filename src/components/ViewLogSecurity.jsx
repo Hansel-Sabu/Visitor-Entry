b44d2c1f062d123296c2cb4f8bcf1ab0bc910a32
Liyo08 <liyotom04@gmail.com>
2025-01-26 22:47:39 +0530

    viewlogsecurity branch created

--- src/components/ViewLogSecurity.jsx
+++ src/components/ViewLogSecurity.jsx
@@ -7,6 +7,21 @@ const ViewLogSecurity = () => {
 
             <SecurityNavbar />
 
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+
+
+
+
+
+
+
+                    </div>
+                </div>
+            </div>
+
 
 
 
