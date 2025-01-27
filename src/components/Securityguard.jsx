29f8e88df7afea07ca911b0f36b1ae1244cdae06
chinmayasuresh <chinmayasuresh12@gmail.com>
2025-01-27 20:25:30 +0530

    username created

--- src/components/Securityguard.jsx
+++ src/components/Securityguard.jsx
@@ -5,7 +5,54 @@ const Securityguard = () => {
   return (
     <div>
       <NavbarHome />
-      Security Login Page
+      <div className="container">
+        <div className="row">
+          <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+          <div class="card" style="width: 18rem;">
+  <img> src="..." class="card-img-top" alt="..."</img>
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+            <div className="row g-3">
+              <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <label htmlFor="" className="form_label"><b>User Name</b></label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+              <label htmlFor="" className="form_label"><b>Password</b></label>
+              <input type="password" className="form-control" />
+              </div>
+              <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <button className="btn btn-success"><b>Login</b></button>
+              </div>
+              <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+              <button className="btn btn-danger"><b>forgot password</b></button>
+              </div>
+            </div>
+
+
+
+
+
+
+          </div>
+        </div>
+      </div>
 
     </div>
   )
