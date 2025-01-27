d4c688ed509e47fa21c80e110b839fdaaef5e1c7
chinmayasuresh <chinmayasuresh12@gmail.com>
2025-01-27 21:44:00 +0530

    card inserted

--- src/components/Securityguard.jsx
+++ src/components/Securityguard.jsx
@@ -8,7 +8,13 @@ const Securityguard = () => {
       <div className="container">
         <div className="row">
           <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-          <div className="row g-3">
+            <center>
+              <div class="card" >
+                <h5 class="card-title"><b>SECURITY LOGIN</b></h5>
+                <img height="300px" width="" src="https://t3.ftcdn.net/jpg/03/39/70/90/360_F_339709048_ZITR4wrVsOXCKdjHncdtabSNWpIhiaR7.jpg" class="card-img-top" alt="..."></img>
+                <div class="card-body">
+                  <p class="card-text">
+                    <div className="row g-3">
                       <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                         <label htmlFor="" className="form_label"><b>User Name</b></label>
                         <input type="text" className="form-control" />
@@ -20,23 +26,16 @@ const Securityguard = () => {
                       <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                         <button className="btn btn-success"><b>Login</b></button>
                       </div>
-                     </div>
-                     </div>
-                     </div>
-
-
-
-
-
-
-
-
-
-
-
+                    </div>
+                  </p>
+                  <a href="#" class="btn btn-primary"><b>Forgot Password</b></a>
+                </div>
+              </div>
+            </center>
           </div>
-
-    </div>
+        </div>
+      </div>
+    </div >
   )
 }
 
