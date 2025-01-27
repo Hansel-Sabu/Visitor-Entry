91b5ad1351b9b98f427c32cbcb453d53b59ef86a
chinmayasuresh <chinmayasuresh12@gmail.com>
2025-01-27 20:51:44 +0530

    card inserted

--- src/components/Securityguard.jsx
+++ src/components/Securityguard.jsx
@@ -8,15 +8,29 @@ const Securityguard = () => {
       <div className="container">
         <div className="row">
           <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <center>
+              <div class="card" >
+              <h5 class="card-title"><b>SECURITY LOGIN</b></h5>
+                <img height="300px" width="" src="https://t3.ftcdn.net/jpg/03/39/70/90/360_F_339709048_ZITR4wrVsOXCKdjHncdtabSNWpIhiaR7.jpg" class="card-img-top" alt="..."></img>
+                <div class="card-body">
+                  
+                  <p class="card-text">
+                    <div className="row g-3">
+                      <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <label htmlFor="" className="form_label"><b>User Name</b></label>
+                        <input type="text" className="form-control" />
+                      </div>
+                      <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <label htmlFor="" className="form_label"><b>Password</b></label>
+                        <input type="password" className="form-control" />
+                      </div>
+                      <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <button className="btn btn-success"><b>Login</b></button>
+                      </div>
 
-          <div class="card" style="width: 18rem;">
-  <img> src="..." class="card-img-top" alt="..."</img>
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
+                    </div>
+                  </p>
+                  <a href="#" class="btn btn-primary"><b>Forgot Password</b></a>
 
 
 
@@ -25,25 +39,20 @@ const Securityguard = () => {
 
 
 
-
-
-
-            <div className="row g-3">
-              <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                <label htmlFor="" className="form_label"><b>User Name</b></label>
-                <input type="text" className="form-control" />
+                </div>
               </div>
-              <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-              <label htmlFor="" className="form_label"><b>Password</b></label>
-              <input type="password" className="form-control" />
-              </div>
-              <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                <button className="btn btn-success"><b>Login</b></button>
-              </div>
-              <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-              <button className="btn btn-danger"><b>forgot password</b></button>
-              </div>
-            </div>
+            </center>
+
+
+
+
+
+
+
+
+
+
+
 
 
 
