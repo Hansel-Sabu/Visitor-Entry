ffe92660eacae5e79d42767d468b93a1ecbb035c
Xtinaps <xtinasparakal@gmail.com>
2025-01-27 19:05:35 +0530

    created

--- src/components/ViewDailyLogs.jsx
+++ src/components/ViewDailyLogs.jsx
@@ -1,18 +1,61 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import AdminNavbar from './AdminNavbar'
+import axios from 'axios'
+
+
 
 const ViewDailyLogs = () => {
-    return (
-        <div>
+    const[data,changeData]=useState([])
 
-            <AdminNavbar />
+    const fetchData=()=>{
+        axios.get("/api/admin/view-logs", {
+            headers: {
+                'Authorization': 'Bearer jwt_token'
+            }
+        }).then(
+            (response)=>{
+                changeData(response.data)
+            }
+        ).catch()
+    }
+        
+   useEffect(
+    ()=>{fetchData()},[]
+   )    
+  return (
+    <div>
+        <AdminNavbar/>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <table className="table">
+  <thead>
+    <tr>
+      <th scope="col">Date</th>
+      <th scope="col">Visitor Name</th>
+      <th scope="col">Purpose</th>
+          </tr>
+  </thead>
+  <tbody>
 
-
-
-
-
+   {data.map(
+    (value,index)=>{
+        return <tr key={index}>
+        
+        <td>{value.date}</td>
+        <td>{value.visitorName}</td>
+        <td>{value.purpose}</td>
+              </tr>
+    }
+   )}
+    
+  </tbody>
+</table>
+                </div>
+            </div>
         </div>
-    )
+    </div>
+  )
 }
 
 export default ViewDailyLogs
