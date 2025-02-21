f591a5dede770451e411b67f873cdd771b4723e7
arjunsunil-boop <arjunsunilgokulan@gmail.com>
2025-02-21 21:22:00 +0530

    Search Api added

--- src/components/SearchSecurity.jsx
+++ src/components/SearchSecurity.jsx
@@ -11,7 +11,7 @@ const SearchSecurity = () => {
 
     const [searchItem,setSearchItem]=useState(
         {
-            "searchKey" :""
+            "name" :""
         }
     )
 
@@ -22,25 +22,29 @@ const SearchSecurity = () => {
     }
 
     const submitSearch = (e)=>{
-        console.log(searchItem)
-
         e.preventDefault();
-        axios.post("apilink",searchItem,{
-            headers:{'token':token, "Content-Type": "application/json" }}).then(
+        axios.post("http://18.144.111.41:3000/api/admin/search-security",searchItem,{
+            headers:{'Authorization':`Bearer ${token}`, "Content-Type": "application/json" }}).then(
                 (response)=>{
                     changeSecurity(response.data)
+                    
                 }
             ).catch(
                 (error)=>{
+
                     console.log(error)
+                    if (error.response.status === 401) {
+                        alert("Login Again")
+                    } else if(error.response.status === 400) {
+                        alert("Name field empty!!")
+                    }
+                    
                 }
+                
             )
 
 
     }
-
-
-
 
     return (
         <div>
@@ -53,7 +57,7 @@ const SearchSecurity = () => {
                         <div className="row g-3">
                             <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                                 <label htmlFor="" className="form-label">Search for security guard</label>
-                                <input type="text" className="form-control" name='searchKey' value={searchItem.searchKey} onChange={searchHandle} />
+                                <input type="text" className="form-control" name='name' value={searchItem.name} onChange={searchHandle} />
                             </div>
                             <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                                 <button className="btn btn-success" onClick={submitSearch}>Search</button>
@@ -66,8 +70,14 @@ const SearchSecurity = () => {
                                 <thead>
                                     <tr>
 
-                                        <th scope="col">ID</th>
-                                        <th scope="col">Username</th>
+                                        <th scope="col">Name</th>
+                                        <th scope="col">Email</th>
+                                        <th scope="col">Phone No</th>
+                                        <th scope="col">Employee Code</th>
+                                        <th scope="col">Address</th>
+                                        <th scope="col">DOB</th>
+                                        <th scope="col">Active Status</th>
+                                        <th scope="col">Date Created</th>
 
                                     </tr>
                                 </thead>
@@ -80,8 +90,21 @@ const SearchSecurity = () => {
 
                                                     <tr>
 
-                                                        <td>{i._id}</td>
-                                                        <td>{i.username}</td>
+                                                        <td>{i.name}</td>
+                                                        <td>{i.email}</td>
+                                                        <td>{i.phone_no}</td>
+                                                        <td>{i.employee_code}</td>
+                                                        <td>{i.address}</td>
+                                                        <td>{new Date(i.date_of_birth).toLocaleDateString()}</td>
+                                                        <td>{(()=>{
+                                                            if (i.is_active) {
+                                                                return 'Active'
+                                                            }
+                                                            else{
+                                                                return 'Inactive'
+                                                            }
+                                                        })()}</td>
+                                                        <td>{new Date(i.created_date).toLocaleDateString()}</td>
 
                                                     </tr>
 
